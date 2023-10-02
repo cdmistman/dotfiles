a57b9ff99f82c82dd9b976d7b5b0270b92bcdaa9
Colton Donnelly <colton@donn.io>
2023-10-02 02:03:49 -0400

    nvim: just use a single lua file, collapse inputs

--- home/colton/programs/neovim/default.nix
+++ home/colton/programs/neovim/default.nix
@@ -1,19 +1,34 @@
 {
+  inputs,
   pkgs,
+  system,
   ...
 }:
 
-{
-  imports = [
-    ./plugins
+let
+  raw-plugins = with inputs; [
+    { name = "auto-save.nvim"; src = auto-save-nvim; }
+    { name = "comment.nvim"; src = comment-nvim; }
+    { name = "nvim-cmp"; src = inputs.nvim-cmp; }
+    { name = "nvim-lspconfig"; src = nvim-lspconfig; }
+    { name = "nvim-treesitter-context"; src = nvim-treesitter-context; }
+    { name = "neo-tree.nvim"; src = neo-tree-nvim; }
+    { name = "rust-tools.nvim"; src = rust-tools-nvim; }
+    { name = "telescope.nvim"; src = telescope-nvim; }
+    { name = "twilight-nvim"; src = twilight-nvim; }
+    { name = "which-key.nvim"; src = which-key-nvim; }
   ];
 
+  plugins = builtins.map pkgs.vimUtils.buildVimPluginFrom2Nix raw-plugins;
+in
+
+{
   programs.neovim = {
     enable = true;
 
     defaultEditor = true;
 
-    plugins = with pkgs.vimPlugins; [
+    plugins = plugins ++ (with pkgs.vimPlugins; [
       cmp-nvim-lsp
       cmp-nvim-lsp-document-symbol
       cmp-nvim-lsp-signature-help
@@ -26,7 +41,7 @@
       plenary-nvim
 
       vim-vsnip
-    ];
+    ]);
 
     withNodeJs = true;
 
@@ -35,5 +50,15 @@
     extraLuaConfig = ''
       ${builtins.readFile ./init.lua}
     '';
+
+    extraPackages =
+      let
+        inherit (inputs.fenix.packages.${system}) rust-analyzer;
+
+        inherit (pkgs.nodePackages_latest) typescript-language-server;
+      in [
+        rust-analyzer
+        typescript-language-server
+      ];
   };
 }
--- home/colton/programs/neovim/init.lua
+++ home/colton/programs/neovim/init.lua
@@ -8,3 +8,156 @@ vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.signcolumn = "yes"
 vim.opt.termguicolors = true
+
+local which_key = require('which-key')
+which_key.setup()
+
+require('auto-save').setup()
+
+local cmp = require('cmp')
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		[','] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	}),
+	snipped = {
+		expand = function(args)
+			vim.fn['vsnip#anonymous'](args.body)
+		end
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim-lsp' },
+		{ name = 'vsnip' }
+	}, {
+		name = 'buffer',
+	}),
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+})
+
+require('Comment').setup()
+
+do
+	local lsp = require('lspconfig')
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+	local on_attach = function(client, buffer)
+		which_key.register({
+			g = {
+				name = "Go",
+				["."] = {
+					"<cmd>lua vim.lsp.buf.definition()<cr>",
+					"Definition",
+				},
+				d = {
+					"<cmd>lua vim.lsp.buf.declaration()<cr>",
+					"Declaration",
+				},
+				D = {
+					"<cmd>lua vim.lsp.buf.type_definition()<cr>",
+					"Type definition",
+				},
+				h = {
+					"<cmd>lua vim.lsp.buf.hover()<cr>",
+					"Hover",
+				},
+				i = {
+					"<cmd>lua vim.lsp.buf.implementation()<cr>",
+					"Implementation",
+				},
+				r = {
+					"<cmd>lua vim.lsp.buf.references()<cr>",
+					"References",
+				},
+				s = {
+					"<cmd>lua vim.lsp.buf.signature_help()<cr>",
+					"Signature help",
+				},
+				n = {
+					"<cmd>lua vim.lsp.diagnostic.goto_next()<cr>",
+					"Next diagnostic",
+				},
+				p = {
+					"<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
+					"Previous diagnostic",
+				},
+			},
+
+			["<leader>c"] = {
+				name = "Code",
+				a = {
+					"<cmd>lua vim.lsp.buf.code_action()<cr>",
+					"Code action",
+				},
+				f = {
+					"<cmd>lua vim.lsp.buf.formatting()<cr>",
+					"Format",
+				},
+				r = {
+					"<cmd>lua vim.lsp.buf.rename()<cr>",
+					"Rename",
+				},
+			},
+		}, {
+			buffer = buffer,
+		})
+	end
+
+	lsp.tsserver.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		cmd = { "typescript-language-server", "--stdio" },
+	})
+
+	require('rust-tools').setup({
+		server = {
+			capabilities = capabilities,
+			on_attach = on_attach,
+			cmd = { "rust-analyzer" },
+		},
+	})
+end
+
+require('neo-tree').setup({
+	sort_case_insensitive = true,
+	default_component_configs = {
+		indent = {
+			indent_size = 1,
+		},
+	},
+})
+
+require('telescope').setup()
+which_key.register({
+	["<leader>f"] = {
+		name = "Find",
+		f = {
+			"<cmd>lua require('telescope.builtin').find_files()<cr>",
+			"Files",
+		},
+		g = {
+			"<cmd>lua require('telescope.builtin').live_grep()<cr>",
+			"Grep",
+		},
+		b = {
+			"<cmd>lua require('telescope.builtin').buffers()<cr>",
+			"Buffers",
+		},
+		h = {
+			"<cmd>lua require('telescope.builtin').help_tags()<cr>",
+			"Help",
+		},
+	},
+})
+
+require('treesitter-context').setup()
+
+require('twilight').setup()
+vim.api.nvim_create_autocmd({ "UIEnter" }, {
+	command = "TwilightEnable",
+})
