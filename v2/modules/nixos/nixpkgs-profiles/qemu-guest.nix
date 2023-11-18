263b9bc0a9d3f4dad812bfedffeae6236413e222
Colton Donnelly <colton@donn.io>
2023-11-18 18:11:02 +0100

    v2: fix nixpkgs-profiles.qemu-guest again

--- v2/modules/nixos/nixpkgs-profiles/qemu-guest.nix
+++ v2/modules/nixos/nixpkgs-profiles/qemu-guest.nix
@@ -5,12 +5,11 @@ args @ {
   ...
 }:
 with lib; let
-  cfg = config.nixpkgs-profiles.qemu-guest.enable;
+  cfg = config.nixpkgs-profiles.qemu-guest;
   modPath = modulesPath + "/profiles/qemu-guest.nix";
   mod = import modPath args;
 in {
   options.nixpkgs-profiles.qemu-guest.enable = mkEnableOption (mdDoc "Enable QEMU guest profile from nixpkgs.");
 
-  config = mkIf cfg.enable (builtins.trace mod mod);
-  # config = {};
+  config = mkIf cfg.enable mod;
 }
