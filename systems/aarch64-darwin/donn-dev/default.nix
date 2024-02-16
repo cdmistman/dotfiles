8fb9fb01a8afe3c1d4851b9b46892059919a16ad
Colton Donnelly <colton@donn.io>
2024-02-15 19:18:56 -0500

    systems/donn-dev: add commptonn user

--- systems/aarch64-darwin/donn-dev/default.nix
+++ systems/aarch64-darwin/donn-dev/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   mistman = {
     owned.enable = true;
     server.enable = true;
@@ -6,6 +6,20 @@
 
   # NOTE: append-only
   users.knownGroups = [ "colton" ];
-  users.knownUsers = [ "colton" ];
+  users.knownUsers = [ "colton" "commptonn" ];
+
+  users.users.commptonn = {
+    createHome = true;
+    description = "commptonn";
+    gid = 20;
+    home = "/home/commptonn";
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArYAmI4x/5cPMc1D3EkOMfDex9F8LOoe3Mnap04JZv4 root@ignite"
+    ];
+
+    shell = "${pkgs.zsh}/bin/zsh";
+    uid = 602;
+  };
 }
 
