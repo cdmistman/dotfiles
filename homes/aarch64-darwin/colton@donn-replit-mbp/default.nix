0f6c4c7cf1206e728a0fb09d4f3a5589114bf094
Colton Donnelly <colton@donn.io>
2024-03-25 09:56:24 -0400

    donn-replit-mbp: update after refactors

--- homes/aarch64-darwin/colton@donn-replit-mbp/default.nix
+++ homes/aarch64-darwin/colton@donn-replit-mbp/default.nix
@@ -1,7 +1,23 @@
-{
-  mistman = {
-    profile.enable = true;
-    gui-apps.enable = true;
-    work.enable = true;
+{pkgs, ...}: {
+  mistman.profile = {
+    enable = true;
+    gui-apps = true;
+  };
+
+  home = {
+    packages = with pkgs; [
+      docker
+    ];
+
+    # TODO: get nixpkgs-packaged google-cloud-sdk working
+    sessionPath = [
+      "$HOME/.google-cloud-sdk/bin"
+    ];
+
+    sessionVariables = {
+      CLOUDSDK_PYTHON = "${pkgs.python3.withPackages (ps: with ps; [numpy])}/bin/python3";
+      CLOUDSDK_PYTHON_SITEPACKAGES = "1";
+      CLOUDSDK_LOCATION = "$HOME/.google-cloud-sdk";
+    };
   };
 }
--- systems/aarch64-darwin/donn-replit-mbp/default.nix
+++ systems/aarch64-darwin/donn-replit-mbp/default.nix
@@ -1,23 +1,11 @@
 {pkgs, ...}: {
-  mistman = {
-    macbook.enable = true;
-    owned.enable = true;
+  mistman.profile.enable = true;
+
+  users.users.admin = {
+    createHome = true;
   };
 
-  home = {
-    packages = with pkgs; [
-      docker
-    ];
-
-    # TODO: get nixpkgs-packaged google-cloud-sdk working
-    sessionPath = [
-      "$HOME/.google-cloud-sdk/bin"
-    ];
-
-    sessionVariables = {
-      CLOUDSDK_PYTHON = "${pkgs.python3.withPackages (ps: with ps; [numpy])}/bin/python3";
-      CLOUDSDK_PYTHON_SITEPACKAGES = "1";
-      CLOUDSDK_LOCATION = "$HOME/.google-cloud-sdk";
-    };
+  users.users.colton = {
+    createHome = false;
   };
 }
